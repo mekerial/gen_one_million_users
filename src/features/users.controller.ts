677254c2d4c97d2d3e7bfb54cd157e44f681e2ce
MEKERIAL <mekery00@gmail.com>
2024-11-17 01:35:34 +0300

    refactor in controller with comments

--- src/features/users.controller.ts
+++ src/features/users.controller.ts
@@ -5,17 +5,18 @@ import { UsersService } from './users.service';
 export class UsersController {
   constructor(protected usersService: UsersService) {}
 
+  //generating 1000000 users
+  @Post('generate')
+  async generateUsers() {
+    await this.usersService.generateUsers();
+    return;
+  }
+  //reset problems
   @Post('reset-problems')
   async resetProblemsFlag() {
     const count = await this.usersService.updateProblemsFlag();
     return {
       message: `Was problems: ${count}`,
     };
-  }
-
-  @Post('generate')
-  async generateUsers() {
-    await this.usersService.generateUsers();
-    return;
   }
 }
